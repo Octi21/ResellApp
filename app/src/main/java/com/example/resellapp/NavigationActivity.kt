39df102824f0093cab27e1734e8cd9789b5c9a3c
Octi21 <80587811+Octi21@users.noreply.github.com>
2023-04-27 14:44:49 +0300

    users added to realtimeDatabase

--- app/src/main/java/com/example/resellapp/NavigationActivity.kt
+++ app/src/main/java/com/example/resellapp/NavigationActivity.kt
@@ -1,17 +1,32 @@
 package com.example.resellapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.resellapp.databinding.ActivityNavigationBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
 
 class NavigationActivity: AppCompatActivity() {
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
+    private lateinit var database: FirebaseDatabase
+
+    private lateinit var dbRef: DatabaseReference
+
+
+
 
     private lateinit var binding: ActivityNavigationBinding
 
@@ -24,8 +39,43 @@ class NavigationActivity: AppCompatActivity() {
         setContentView(binding.root)
 
 
+
         val bundle = Bundle()
         bundle.putString("email", intent.getStringExtra("email"))
+
+
+
+        firebaseAuth = Firebase.auth
+        val user = firebaseAuth.currentUser
+
+        val uid = user!!.uid
+        val email = user?.email
+        val name = user?.displayName
+
+        Log.e("userInfo","${uid} ${name} ${email}")
+
+        database = FirebaseDatabase.getInstance("https://androidkotlinresellapp-default-rtdb.europe-west1.firebasedatabase.app/")
+
+        dbRef = database.getReference("Users")
+
+        dbRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.hasChild(uid))
+                {
+
+                }
+                else
+                {
+                    val user = User(uid,name,email)
+                    dbRef.child(uid).setValue(user)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("userserror", "loadItems:onCancelled", error.toException())
+            }
+        })
+
 
 
         val navController = this.findNavController(R.id.myNavHostFragment)
--- app/src/main/java/com/example/resellapp/home/HomeFragment.kt
+++ app/src/main/java/com/example/resellapp/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.resellapp.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,13 +9,27 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.resellapp.R
 import com.example.resellapp.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment: Fragment() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         val binding: FragmentHomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home,container,false)
+
+
+//        firebaseAuth = Firebase.auth
+//        val user = firebaseAuth.currentUser
+//
+//        val uid = user?.uid
+////        val name = user?.
+//
+//        Log.e("userInfo","${uid}")
 
 
 
